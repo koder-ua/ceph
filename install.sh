d9c3f75fe7c5ed4a43f6837307afa2cea4504e63
Vitalii Solovei <vsolovei@mirantis.com>
2015-09-10 16:05:24 +0300

    in progress

--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ CLIENTS="client_host1 client_host2"
 OSDS="osd_host1 osd_host2 osd_host3"
 
 
-
-######### on the node ###########
-#    cd /root/
-# mkdir dst_ceph/
-
   DST='/root/dst_ceph'
   mkdir -p $DST
   cd $DST
@@ -19,15 +14,38 @@ OSDS="osd_host1 osd_host2 osd_host3"
     cp -rf $DST/ceph/metadata.json $DST/puppet-ceph
     cp -rf $DST/ceph/Puppetfile $DST/puppet-ceph
 
+    rm -rf $DST/ceph
+    mv puppet-ceph ceph
+
+ tar cfvz ceph.tar.gz ceph
 
 
-   mv puppet-ceph ceph
-   cd puppet-ceph/
+##############################
+#    for i in $MONS; do      #
+#        echo "MONS=$i";     #
+#        done                #
+##############################
 
- sudo apt-get install ruby build-essential ruby1.9.1 ruby1.9.1-dev libxml2 zlib zlib-bin zlib1g zlib1g-dev
- sudo gem install bundler
- bundle install
+#############################################
+#       copy ceph.tar.gz to nodes           #
+#             untar tarball                 #
+#############################################
 
+
+ mkdir -p /etc/puppetlabs/code/modules/
+   cp -rf ceph /etc/puppetlabs/code/modules/
+   cd /etc/puppetlabs/code/modules/ceph
+
+
+ apt-get install ruby build-essential ruby1.9.1 ruby1.9.1-dev libxml2 zlib-bin zlib1g zlib1g-dev
+
+################################################################
+#                   need investigate                           #
+# cat Gemfile | grep -w gem | sed "s/,/ /g" | awk {'print $2'} #
+################################################################
+
+    sudo gem install bundler
+    bundle install
 
         # Install Puppet modules
 
@@ -38,6 +56,7 @@ OSDS="osd_host1 osd_host2 osd_host3"
      puppet module install puppetlabs-concat
      puppet module install puppetlabs-firewall
 
- cp -rf ceph /etc/puppet/modules/
+
+###############################################################
 
      puppet apply /tmp/ceph.puppet
